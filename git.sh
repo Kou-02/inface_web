0bca7a74b38649dc1bc835e40fcdbbf2490d0dda
abdul faheem a <abdulfaheemasd@gmail.com>
2022-04-09 15:30:45 +0530

    "asd"

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 git add .
 echo "enter the message for commit:"
 read message
-echo "$message"
-git commit -m "$message"
+echo '"'$message'"'
+git commit -m '"'$message'"' #this converts message with collen
 git push
 git status
