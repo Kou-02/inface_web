7d23d063bc8a027c7b6a7650e240aa1741ce1cba
abdul faheem a <abdulfaheemasd@gmail.com>
2022-04-09 20:40:00 +0530

    login page is generated

--- git.sh
+++ git.sh
@@ -2,7 +2,9 @@
 git add .
 echo "enter the message for commit:"
 read message
-echo '"'$message'"'
-git commit -m '"'$message'"' #this converts message with collen
+echo "$message"
+git commit -m "$message" #this converts message with collen
 git push
 git status
+
+#admin user name admin and password admin
--- inface/facein/views.py
+++ inface/facein/views.py
@@ -1,8 +1,36 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, StreamingHttpResponse
+from django.views.decorators import gzip
+import cv2
+from django.contrib.auth.models import User
+from .models import *
+from django.core.mail import EmailMessage
+import sqlite3
+from sre_constants import SUCCESS
+from tabnanny import check
+import cv2
+import numpy as np
+import face_recognition as fc
+import os
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import UserCreationForm
+
+@gzip.gzip_page
 
 # Create your views here.
 
 def index(request):
     return render(request, 'facein/index.html')
+
+#def facerec(request):
+    
+
+
+def login(request):
+    form = UserCreationForm()
+    return render(request, "facein/login.html",)
+
+#def logout(request):
+
+#def profile(request):
