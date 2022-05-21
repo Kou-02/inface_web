b344d184201c668077ae567d306d00ac684568fd
Your Name <you@example.com>
2022-05-21 12:28:01 +0530

    face scanning complete only attendance is need to be linked

--- inface/facein/views.py
+++ inface/facein/views.py
@@ -1,36 +1,34 @@
-#import cv2 #koushik
+from re import T
+from tracemalloc import start
+import cv2 #koushik
+from msilib.schema import AdminExecuteSequence
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, StreamingHttpResponse
 from django.views.decorators import gzip
 from django.contrib.auth.models import User
-# from .models import Profile
+from .models import Profile as pro
 from django.core.mail import EmailMessage
-#import sqlite3
+import sqlite3
 from sre_constants import SUCCESS
 from tabnanny import check
-#import cv2
 import numpy as np
-#import face_recognition as fc #koshik
-#import os
+import face_recognition as fc #koshik
+import os
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.conf.urls.static import static
-
-#import cv2
-#from django.contrib.auth.models import User
-#from .models import *
-#from django.core.mail import EmailMessage
-#import sqlite3
-#from sre_constants import SUCCESS
-#from tabnanny import check
-##import cv2
-#import numpy as np
-#import face_recognition as fc
-#import os
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+from django.core.mail import EmailMessage
+from sre_constants import SUCCESS
+from tabnanny import check
+import numpy as np
+import face_recognition as fc
+import os
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login as auth_login
-#from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 @gzip.gzip_page
 
@@ -40,13 +38,42 @@ def index(request):
     return render(request, 'facein/index.html')
 
 def facerec(request):
-    #cap = cv2.VideoCapture(0)
-    print ("the program is running")
-    i=0
-    # while i
-    # print (Profile.objects.id_no)
-    
-    #you where going to get the sql data to start the face scanning
+    start = 2
+    end = 3
+    i=start
+    count=pro.objects.all().count()
+    print(count)
+    while True:
+        if i <=end:
+            loc=pro.objects.filter(id= i).values_list('Profile_picture').first()[0]
+            print (loc)
+            i+=1
+            cwd = os.getcwd()
+            path=os.path.join(cwd,'media',loc).replace("\\","/")
+            img = cv2.imread(path)
+            img= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            facescurframe = fc.face_locations(img)
+            encode = fc.face_encodings(img,facescurframe)
+            print(encode)
+            cap = cv2.VideoCapture(0)
+            break_out_flag = False
+
+                #
+            SUCESS,img = cap.read()
+            imgs = cv2.resize(img,(0,0),None,0.25,0.25)
+            imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
+            facescurframe = fc.face_locations(img)
+            encode2=fc.face_encodings(img,facescurframe)
+            for encodeface,faceloc in zip(encode2,facescurframe):
+                match = fc.compare_faces(encode,encodeface)
+                if match[0] == True:
+                    print("hehehe")
+                    break
+
+        else:
+            i=start
+                    
+            #you where going to get the sql data to start the face scanning
 
 
     return render(request,"facein/facerec.html", {'title': "face",'id':id})
